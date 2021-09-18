e6978a7f3de4aa86deea993a292f2f2266246fab
GaGa-Kim <wn8925@sookmyung.ac.kr>
2021-09-18 22:20:48 +0900

    210918

---
+++ Bronze IV/14470.py
@@ -0,0 +1,11 @@
+A = int(input())
+B = int(input())
+C = int(input())
+D = int(input())
+E = int(input())
+
+if A < 0:
+    print(C*(B-abs(A)))
+elif A == 0:
+    print()
+else:
---
+++ Bronze IV/14623.py
@@ -0,0 +1,4 @@
+B1 = int(input(), 2)
+B2 = int(input(), 2)
+B3 = format((B1 * B2), 'b')
+print(B3)
